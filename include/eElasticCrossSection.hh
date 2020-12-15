0a293419bb4828698e2b3f2a3cb94bc1c5d4bd28
David Flay <flay@enpflay.jlab.org>
2020-12-15 16:45:21 -0600

    Added the elastic cross section class

---
+++ include/eElasticCrossSection.hh
@@ -0,0 +1,36 @@
+#ifndef eELASTIC_CROSS_SECTION_HH
+#define eELASTIC_CROSS_SECTION_HH
+
+// eElasticCrossSection class
+// Notes: - Kinematic variables: beam energy (Es) and scattering angle (th) is in degrees.   
+//        - Target variables: Z, A 
+//        - The resulting cross section is for elastic electron scattering  
+
+#include <cstdlib> 
+#include <iostream>
+#include <iomanip> 
+#include <cmath>
+
+#include "constants.hh"
+#include "Kinematics.hh"
+#include "ElasticFormFactor.hh"
+#include "eInclusiveCrossSection.hh"
+
+class eElasticCrossSection: public eInclusiveCrossSection  {
+
+   private: 
+      ElasticFormFactor *fFormFactor; 	
+
+   public: 
+      eElasticCrossSection(double Z=1,double A=1);
+      ~eElasticCrossSection();
+
+      void SetTargetParameters(double Z,double A){ fZ = Z; fA = A; }
+      
+      void SetFormFactor(ElasticFormFactor *ff) { fFormFactor = ff; }
+       
+      double GetBornXS();
+
+};
+
+#endif
---
+++ src/eElasticCrossSection.cc
@@ -0,0 +1,38 @@
+#include "eElasticCrossSection.hh"
+//______________________________________________________________________________
+eElasticCrossSection::eElasticCrossSection(double Z,double A){
+   fZ  = Z;
+   fA  = A;
+}
+//______________________________________________________________________________
+eElasticCrossSection::~eElasticCrossSection(){
+
+}
+//______________________________________________________________________________
+double eElasticCrossSection::GetBornXS(){
+   // Es = incident electron energy
+   // th = scattered electron angle
+   double MT    = fA*proton_mass; 
+   double thr   = fTh*deg_to_rad;
+   double TAN   = tan(thr/2.); 
+   double TAN2  = TAN*TAN;
+   // kinematics and form factors 
+   fEp = Kinematics::GetEp_Elastic(fEs,fTh,MT);  // scattered electron energy 
+   double Q2    = Kinematics::GetQ2(fEs,fEp,fTh);
+   double tau   = Q2/(4.*MT*MT);
+   double GE    = fFormFactor->GetGE(Q2);
+   double GM    = fFormFactor->GetGM(Q2);
+   double W1    = tau*GM*GM;
+   double W2    = (GE*GE + tau*GM*GM)/(1+tau);
+   // Mott cross section
+   double xs_mott = GetMottXS(fEs,fTh); // in nb  
+   // term 2 
+   double T1=0;
+   double T1_num = fEp;
+   double T1_den = fEs;
+   if(T1_den!=0) T1 = T1_num/T1_den;
+   // term 3 
+   double T2     = W2 + 2.*TAN2*W1;
+   double xs_el  = xs_mott*T1*T2;
+   return xs_el;
+}
